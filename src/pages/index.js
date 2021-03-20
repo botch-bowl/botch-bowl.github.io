f37c83e3115730cfc39fa1da11bceda106a323a5
Felix Schett <felix.schett@student.uibk.ac.at>
2021-03-20 01:14:16 +0100

    StaticImage tets

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import * as React from 'react';
 import Layout from 'src/components/Layout';
 import Seo from 'src/components/Seo';
 import { Button, Heading } from '@chakra-ui/react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Heading as="h1" size="xl">Hello.</Heading>
+    <Heading as="h1" size="xl">
+      Hello.
+    </Heading>
     <Button>Hello</Button>
+    <StaticImage src="../assets/images/unsplash-example.jpg" alt="example" loading="eager" />
   </Layout>
 );
 
