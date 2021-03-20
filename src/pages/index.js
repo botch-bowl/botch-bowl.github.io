3d5e6be745dd9c8a137afff965dc12d4ce426690
Felix Schett <felix.schett@student.uibk.ac.at>
2021-03-20 01:03:34 +0100

    Remove custom heading component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import Layout from 'src/components/Layout';
 import Seo from 'src/components/Seo';
-import Heading from 'src/components/Heading';
-import { Button } from '@chakra-ui/react';
+import { Button, Heading } from '@chakra-ui/react';
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Heading>Hello.</Heading>
+    <Heading as='h1' size='xl'>Hello.</Heading>
     <Button>Hello</Button>
   </Layout>
 );
